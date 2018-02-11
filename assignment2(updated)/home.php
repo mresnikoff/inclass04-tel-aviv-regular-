ba8dc05f2bed02ffff596aca0a1737aff716c820
mresnikoff <35782180+mresnikoff@users.noreply.github.com>
2018-02-11 16:11:41 -0500

    Add files via upload

---
+++ assignment2(updated)/home.php
@@ -0,0 +1,32 @@
+<?php include "inc/html-top.inc"; ?>
+
+
+<?php include "inc/nav.inc"; ?>
+
+	
+		<h2>Welcome to our portfolios!</h2>
+		
+
+	
+<div class="hero-container">
+
+<div class="slider">
+  <img src="images/matt.JPG" alt="Matt">
+  <img src="images/oliver.png" alt="Oliver">
+  <img src="images/albert.png" alt="Albert">
+  
+</div>
+
+</div><!-- .hero-container -->
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="sss/sss.min.js"></script>
+<script>
+$('.slider').sss();
+</script>
+</div><!--.container-->
+
+<script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/menu-highlighter.js"></script>
+</body>
+</html>
---
+++ assignment2(updated)/matt.php
@@ -0,0 +1,38 @@
+<?php include "inc/html-top.inc"; ?>
+
+
+<?php include "inc/nav.inc"; ?>
+
+	<article>
+		<h2>Matt Resnikoff</h2>
+		<img class="right img-frame" src="images/matt.JPG" alt="Fed">
+		<p>My name is Matt Resnikoff. I am currently a Junior at the U of R, majoring in Economics and double minoring in Computer Science and Journalism. 
+			I got into Comp Sci fairly recently, but I have a lot of fun with the coding aspects which is why I decided to start off as the group coder. I really like tennis, both watching and playing. I just recently joined the varsity team after being a part of the club team for the past two and a half years. 
+			My favorite player is Roger Federer (duh). I'm also into watching other pro sports, mainly Football and Basketball.
+			I'm a huge Indianapolis Colts fan so don't try talking to me about football right now since it was a pretty rough year for me.
+			
+
+			Originally, I am from West Long Branch, New Jersey. 
+			That's basically the jersey shore (which has a bad rep for some reason) and it is a wonderful place to grow up. 
+			Being 15 minutes away from the beach is fantastic and the town is nice and small so everyone knows everyone.  
+			I went to Shore Regional High School, our local public school. There I had a pretty decent tennis career.
+			I managed to make all conferance 3 times, the picture of the all conferance team that made the paper my senior year is to the right.
+
+
+
+		</p>
+	</article>
+
+	<aside>
+		<h2>More about me!</h2>
+		<p>On top of being pretty into sports I also am a big comic book guy. I'm a huge fan of all the Marvel movies that've been coming out, not so keen on the DC stuff since it's kind of sucked but I'm a big fan of the DC characters. At the U of R I've taken tons of courses. Those courses consist of The Politics of Sport, Financial Accounting, Web Design, Intro to Programming, Intro to Computer Science, Mobile App Development, Principles of Economics, Intermediate Macro and Micro, Sports Econ, Econometrics, Industrial Organization, Intro to Debate, Reporting and Writing the News, Feature Writing, Creative Writing: Fiction, Editing, Calc 1 and 2, Moral Problems, Elements of Probability and Statistics, and the Rhetoric of Nationalism.</p>
+	</aside>
+
+	
+
+</div><!--.container-->
+
+<script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/menu-highlighter.js"></script>
+</body>
+</html>
