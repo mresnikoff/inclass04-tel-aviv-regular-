81c5304da140c77ecff62546a65b94dd3a1541b3
mresnikoff <35782180+mresnikoff@users.noreply.github.com>
2018-02-11 16:07:23 -0500

    Add files via upload

--- assignment2/home.php
+++ assignment2/home.php
@@ -8,7 +8,22 @@
 		
 
 	
+<div class="hero-container">
 
+<div class="slider">
+  <img src="images/matt.JPG" alt="Matt">
+  <img src="images/oliver.png" alt="Oliver">
+  <img src="images/albert.png" alt="Albert">
+  
+</div>
+
+</div><!-- .hero-container -->
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="sss/sss.min.js"></script>
+<script>
+$('.slider').sss();
+</script>
 </div><!--.container-->
 
 <script src="http://code.jquery.com/jquery.js"></script>
